c61ed121a42f6ddcde9655f10bf0944e147bea78
Danilo Cunha <danilowlc@gmail.com>
2020-11-21 12:36:54 -0300

    desafio finalizado

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -35,6 +35,7 @@ const Import: React.FC = () => {
       await api.post('/transactions/import', data);
       history.push('/');
     } catch (err) {
+      // eslint-disable-next-line no-console
       console.log(err.response.error);
     }
   }
